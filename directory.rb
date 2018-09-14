d237c8608c4fd2a5f2f516beba3a8060a0739734
jebax <joel.eliot.baxter@gmail.com>
2018-09-14 13:54:35 +0100

    Added functionality for input of all different information categories, and support for entering typos.

--- directory.rb
+++ directory.rb
@@ -1,27 +1,75 @@
 # student names are stored in an array.
 def input_students
   students = []
-  puts "Enter a student name. To finish, press enter without entering a name."
 
-  while name = gets.chomp
+  while true do
+    puts "Enter a student name. To finish, press return once before entering a name, or twice at any other time."
+    name = gets.chomp
     break if name.empty?
-    students << {name: name, cohort: :november, nationality: :English, height: "189cm"}
-    puts "Now we have #{students.count} student(s)."
+
+    students << { name: name, cohort: nil, nationality: nil, height: nil }
+    current = students[-1]
+
+    while true do
+      puts "Enter a cohort. To enter the default cohort (November), press return."
+      cohort = gets.chomp
+      if cohort.empty?
+        current[:cohort] = :November
+        break
+      else
+        current[:cohort] = cohort.to_sym
+      end
+
+      puts "Is #{cohort} correct? Type 'no' to re-enter, or anything else to continue."
+      input = gets.chomp
+      break if input != "no"
+    end
+
+    while true do
+      puts "Enter a nationality. To enter the default value (N/A), press return."
+      nationality = gets.chomp
+      if nationality.empty?
+        current[:nationality] = "Undisclosed"
+        break
+      else
+        current[:nationality] = nationality.to_sym
+      end
+
+      puts "Is #{nationality} correct? Type 'no' to re-enter, or anything else to continue."
+      input = gets.chomp
+      break if input != "no"
+    end
+
+    while true do
+      puts "Enter a height in cm. To enter the default value (N/A), press return."
+      height = gets.chomp
+      if height.empty?
+        current[:height] = "Undisclosed"
+        break
+      else
+        current[:height] = "#{height}cm".to_sym
+      end
+
+      puts "Is #{height} correct? Type 'no' to re-enter, or anything else to continue."
+      input = gets.chomp
+      break if input != "no"
+    end
+
   end
 
   students
 end
 
 def print_header
-  puts "The students of Villains Academy".center(60)
-  puts "-----------------------".center(60)
+  puts "The students of Villains Academy".center(100)
+  puts "-----------------------".center(100)
 end
 
 def print(students)
   count = 1
   until count > students.size do
     current = students[count - 1]
-    puts "#{count}. #{current[:name]} (#{current[:cohort]} cohort, #{current[:nationality]}, #{current[:height]})."
+    puts "#{count}. #{current[:name]} (#{current[:cohort]} cohort, #{current[:nationality]}, #{current[:height]}).".center(100)
     count += 1
   end
 end
@@ -31,7 +79,7 @@ def print_specific_initial(students, initial)
     student[:name].chars.first.downcase == initial.downcase
   end
   selected.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort, #{student[:nationality]}, #{student[:height]})."
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort, #{student[:nationality]}, #{student[:height]}).".center(100)
   end
 end
 
@@ -40,15 +88,15 @@ def print_specific_length(students, length)
     student[:name].size <= length
   end
   selected.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort, #{student[:nationality]}, #{student[:height]})."
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort, #{student[:nationality]}, #{student[:height]}).".center(100)
   end
 end
 
 def print_footer(students)
   case students.size
-  when 0 then puts "There are no students!".center(60)
-  when 1 then puts "We only have one student, but they're great!".center(60)
-  else puts "Overall, we have #{students.count} great students.".center(60)
+  when 0 then puts "There are no students!".center(100)
+  when 1 then puts "We only have one student, but they're great!".center(100)
+  else puts "Overall, we have #{students.count} great students.".center(100)
   end
 end
 
